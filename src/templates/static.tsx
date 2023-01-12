3d5936c3103987797b2a29447d1203fc666d9fd7
RocksGithub <121313015+RocksGithub@users.noreply.github.com>
2023-01-12 11:52:42 -0600

    Update static.tsx

--- src/templates/static.tsx
+++ src/templates/static.tsx
@@ -126,7 +126,7 @@ const Static: Template<ExternalImageRenderData> = ({
           </div>
           <div className="space-y-5">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              Rock changed it **** Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
